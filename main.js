c09474e34ebce1de87e13dccccb0664050dede41
ClaPascarella <claudiopascarella1996@gmail.com>
2024-05-07 11:30:42 +0200

    file js e html

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
-team = [
-    {nome: "Wayne Barnett", ruolo: "Founder & CEO", foto: "img/wayne-barnett-founder-ceo.jpg"},
-    {nome: "Angela Caroll", ruolo: "Chief Editor", foto:"img/angela-caroll-chief-editor.jpg"},
-    {nome: "Walter Gordon", ruolo: "Office Manager", foto:"img/walter-gordon-office-manager.jpg"},
-    {nome: "Angela Lopez", ruolo: "Social media Manager", foto:"img/angela-lopez-social-media-manager.jpg"},
-    {nome: "Scott Estrada", ruolo: "Developer", foto:"img/scott-estrada-developer.jpg"},
-    {nome: "Barbara Ramos", ruolo: "Graphic Designer", foto:"img/barbara-ramos-graphic-designer.jpg"},
+let teamList = [
+    {nome: "Wayne Barnett", ruolo: "Founder & CEO", foto: "wayne-barnett-founder-ceo.jpg"},
+    {nome: "Angela Caroll", ruolo: "Chief Editor", foto:"angela-caroll-chief-editor.jpg"},
+    {nome: "Walter Gordon", ruolo: "Office Manager", foto:"walter-gordon-office-manager.jpg"},
+    {nome: "Angela Lopez", ruolo: "Social media Manager", foto:"angela-lopez-social-media-manager.jpg"},
+    {nome: "Scott Estrada", ruolo: "Developer", foto:"scott-estrada-developer.jpg"},
+    {nome: "Barbara Ramos", ruolo: "Graphic Designer", foto:"/barbara-ramos-graphic-designer.jpg"},
 ]
 
-for(let i=0; i<team.length; i++) {
-    console.log(team[i]);
+const cardsContainer = document.getElementById('cards-container');
+
+for (let i = 0; i< teamList.length; i++) {
+    let element = teamList[i];
+    cardsContainer.innerHTML += `
+    <div class="col-4 mb-5">
+        <article class="my_card">
+            <img src="./img/${element.foto}" alt="${element.nome}">
+            <div class="card-description p-2">
+                <h3 class='card-nome mb-1'>
+                    ${element.nome}
+                </h3>
+                <p class='card-foto mb-3'>
+                    ${element.ruolo}
+                </p>
+            </div>
+        </article>
+    </div>`;
 }
